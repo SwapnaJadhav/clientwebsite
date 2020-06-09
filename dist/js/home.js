f07433df5327e5dead787e33007bf782b4372399
SwapnaJadhav <swapnajadhav396@gmail.com>
2020-06-09 19:41:07 +0530

    changes after rejecting
    done

--- dist/js/home.js
+++ dist/js/home.js
@@ -1,10 +1,3 @@
-//   $(document).ready(function(){
-//     $('a.nav-link').click(function(){
-//       $('a.nav-link').removeClass("active");
-//       $(this).addClass("a.nav-link.active");
-//   });
-//   });
-
 $(document).ready(function () {
     $('.nav-link ').click(function(e) {
 
@@ -15,6 +8,20 @@ $(document).ready(function () {
         $(this).addClass("active");
       //  e.preventDefault();
     });
+});
+
+
+$(function(){
+  $('a[href*=\\#]:not([href=\\#])').on('click', function(){
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+    if(target.length){
+      $('html,body').animate({
+        scrolltop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  });
 });
 
 // $('#aboutus1').click(function(){
