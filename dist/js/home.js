3e170f5cefe9129c532ef99b3b878bf60ed1e75a
SwapnaJadhav <swapnajadhav396@gmail.com>
2020-06-11 23:59:04 +0530

    javascript
    scrolling

--- dist/js/home.js
+++ dist/js/home.js
@@ -6,7 +6,9 @@ $(document).ready(function () {
        // var $parent = $(this).parent();
         //$parent.addClass('active');
         $(this).addClass("active");
+        if($(".nav-link.index1_link").length>=1){
        e.preventDefault();
+        
        $('#homelink').click(function(e){
         window.scrollTo(0,-1);
        });
@@ -22,6 +24,7 @@ $(document).ready(function () {
        $('#contactuslink').click(function(e){
         window.scrollTo(0,4200);
        });
+    }
     });
 });
 
